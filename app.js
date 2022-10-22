da8830892fd04125b910893b88ca6dfea799c5e7
Mampionona33 <ramamps33@gmail.com>
2022-10-22 11:11:58 +0300

    create route variables for tours and users

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const express = require('express');
+const { json } = require('express/lib/response');
 require('dotenv').config();
 const fs = require('fs');
+const morgan = require('morgan');
 const app = express();
 
-// app.get('/', (req, res) => {
-//   res.status(200).json({ message: 'Hello from server', app: 'my app' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint...');
-// });
+//  FIRST MIDDLEWARE-------------------------------
+app.use(morgan('dev'));
 
 app.use(express.json());
+
+// midleware
+app.use((req, res, next) => {
+  console.log('hello from middleware');
+  // next is use to execute next middleware
+  next();
+});
+
+// Middleware to add propreties requestTime to the request object
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -42,7 +52,7 @@ const getTourById = (req, res) => {
   const tour = tours.find((el) => el.id === id);
   // if (id > tours.length)
   if (!tour) {
-    return res.status(400).json({
+    return res.status(404).json({
       status: 'fail',
       message: 'Invalid ID',
     });
@@ -57,6 +67,8 @@ const getTourById = (req, res) => {
 };
 
 const getAllTours = (req, res) => {
+  // Callin requestTime middleware
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -101,11 +113,49 @@ const deleteTour = (req, res) => {
   });
 };
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'this route is not yet defined',
+  });
+};
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'this route is not yet defined',
+  });
+};
+const getUserById = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'this route is not yet defined',
+  });
+};
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'this route is not yet defined',
+  });
+};
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'this route is not yet defined',
+  });
+};
+
+const tourRouter = express.Router();
+const userRouter = express.Router();
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+// TOUR ROUTES --------------------------------
 // Create new tour
 // app.post('/api/v1/tours', createTour);
 // Read all tours
 // app.get('/api/v1/tours', getAllTours);
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
+tourRouter.route('/').get(getAllTours).post(createTour);
 
 // Read tour by id
 // app.get('/api/v1/tours/:id', getTourById);
@@ -113,12 +163,15 @@ app.route('/api/v1/tours').get(getAllTours).post(createTour);
 // app.patch('/api/v1/tours/:id', updateTour);
 // Delete data
 // app.delete('/api/v1/tours/:id', deleteTour);
-app
-  .route('/api/v1/tours/:id')
-  .get(getTourById)
-  .patch(updateTour)
-  .delete(deleteTour);
 
+tourRouter.route('/:id').get(getTourById).patch(updateTour).delete(deleteTour);
+// TOUR ROUTES --------------------------------
+
+// USER ROUTES --------------------------------
+userRouter.route('/').get(getAllUsers).post(createUser);
+userRouter.route('/:id').get(getUserById).patch(updateUser).delete(deleteUser);
+
+// START SERVER --------------------------------
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`server start on port :${port}`);
