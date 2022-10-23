e922ad0bd080b4d829b9141ebd747db2dfac109d
Mampionona33 <ramamps33@gmail.com>
2022-10-23 20:13:21 +0300

    configure eslint with .eslintrc.json

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,7 +5,7 @@ const tours = JSON.parse(
 );
 
 exports.checkID = (req, res, next) => {
-  if (parseInt(req.params.id) > tours.length) {
+  if (parseInt(req.params.id, 10) > tours.length) {
     return res.status(404).json({
       status: 'fail',
       message: 'Invalid ID',
@@ -15,15 +15,20 @@ exports.checkID = (req, res, next) => {
 };
 
 exports.checkBody = (req, res, next) => {
-  const name = req.body.name;
-  const duration = req.body.duration;
+  const { name } = req.body.name;
+  const { duration } = req.body.duration;
   if (!name) {
     return res.status(404).json({
       status: 'fail',
-      message: 'Tour must have name',
+      message: 'Name is required',
     });
   }
-  console.log(name);
+  if (!duration) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Duration is required',
+    });
+  }
   next();
 };
 
@@ -41,12 +46,12 @@ exports.uniqueName = (req, res, next) => {
 
 exports.createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
+  const newTour = ({ id: newId }, req.body);
   tours.push(newTour);
   fs.writeFile(
     `${__dirname}/../dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
-    (err) => {
+    () => {
       res.status(201).json({
         status: 'success',
         data: {
@@ -58,7 +63,7 @@ exports.createTour = (req, res) => {
 };
 
 exports.getTourById = (req, res) => {
-  const id = parseInt(req.params.id);
+  const id = parseInt(req.params.id, 10);
   const tour = tours.find((el) => el.id === id);
 
   res.status(200).json({
@@ -71,7 +76,6 @@ exports.getTourById = (req, res) => {
 
 exports.getAllTours = (req, res) => {
   // Callin requestTime middleware
-  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
     results: tours.length,
