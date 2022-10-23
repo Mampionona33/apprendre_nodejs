114d8768efb2aec748daf7c82eeaed7381f6a999
Mampionona33 <ramamps33@gmail.com>
2022-10-23 20:20:34 +0300

    correct lint error

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 
 const morgan = require('morgan');
+
 const app = express();
 
 const tourRouter = require('./routes/tourRoutes');
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // START SERVER --------------------------------
-
 const app = require('./app');
+
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
