2612322c3000a8fd1d12e192a94ca6eca8d02cee
Mampionona33 <ramamps33@gmail.com>
2022-10-23 18:39:36 +0300

    using environment variable

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 //  FIRST MIDDLEWARE-------------------------------
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
 
