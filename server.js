65d3885e54833d110091f723b2dadb61b432dbf5
Mampionona33 <ramamps33@gmail.com>
2022-10-23 22:02:46 +0300

    connect to dataBase

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 // START SERVER --------------------------------
+const mongoose = require('mongoose');
 const app = require('./app');
+
+const DB = process.env.DATA_BASE_URI.replace(
+  '<DATA_BASE_PASSWORD>',
+  process.env.DATA_BASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('DB connection successful'));
 
 const port = process.env.PORT || 8000;
 
